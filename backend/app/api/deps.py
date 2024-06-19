c6a787068406b2c68a2a351daa63e34a3ff529db
Nikita Semenov <nasemenov726@gmail.com>
2024-06-19 17:55:05 +0300

    added initial files for alembic, db, api

---
+++ backend/app/api/deps.py
@@ -0,0 +1,11 @@
+from typing import Annotated, Generator
+from sqlmodel import Session
+
+from app.database import engine
+
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, get_db]
---
+++ backend/app/core/database.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+from pydantic import PostgresDsn
+from pydantic_core import MultiHostUrl
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}'
+
+def SQLALCHEMY_DATABASE_URI() -> PostgresDsn:
+    return MultiHostUrl.build(
+        scheme='postgresql+psycopg',
+        username=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_SERVER'),
+        port=5433,
+        path=os.getenv('POSTGRES_DB')
+    )
+
+engine = create_engine(str(SQLALCHEMY_DATABASE_URI))
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
